6fb19b6abe7594e45893bdd841dc76558261e3f0
Sadikk <youpi.killeur@gmail.com>
2019-09-23 18:15:25 +0200

    [feat] Message properties

--- src/main/java/fr/mifa/core/models/Message.java
+++ src/main/java/fr/mifa/core/models/Message.java
@@ -1,5 +1,33 @@
 package fr.mifa.core.models;
 
 public abstract class Message extends Model {
+    private int roomId;
 
+    private int authorId;
+
+    private String authorName;
+
+    public int getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(int roomId) {
+        this.roomId = roomId;
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
 }
