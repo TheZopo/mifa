b9f64cc1ad6f075ae5991151443d6bb1b02ce557
Sadikk <youpi.killeur@gmail.com>
2019-09-26 19:11:03 +0200

    [fix] joinedRoom packet should have Room instead of id

--- src/main/java/fr/mifa/core/network/protocol/JoinedRoomPacket.java
+++ src/main/java/fr/mifa/core/network/protocol/JoinedRoomPacket.java
@@ -1,16 +1,18 @@
 package fr.mifa.core.network.protocol;
 
+import fr.mifa.core.models.Room;
+
 public class JoinedRoomPacket extends Packet {
-    private int roomId;
+    private Room room;
 
     private String nickname;
 
-    public int getRoomId() {
-        return roomId;
+    public Room getRoom() {
+        return room;
     }
 
-    public void setRoomId(int roomId) {
-        this.roomId = roomId;
+    public void setRoom(Room room) {
+        this.room = room;
     }
 
     public String getNickname() {
@@ -21,8 +23,8 @@ public class JoinedRoomPacket extends Packet {
         this.nickname = nickname;
     }
 
-    public JoinedRoomPacket(String nickname, int roomId) {
-        this.roomId = roomId;
+    public JoinedRoomPacket(String nickname, Room room) {
+        this.room = room;
         this.nickname = nickname;
     }
 }
