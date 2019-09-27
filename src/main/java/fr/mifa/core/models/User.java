7ba28677f1f87d035e5794d4955c468c9d0b451a
Sadikk <youpi.killeur@gmail.com>
2019-09-27 20:08:47 +0200

    [feat] leaving room

--- src/main/java/fr/mifa/core/models/User.java
+++ src/main/java/fr/mifa/core/models/User.java
@@ -7,8 +7,22 @@ public class User extends Model {
 
     private transient PacketManager packetManager;
 
+    private boolean connected;
+
+    public boolean isConnected() {
+        return connected;
+    }
+
+    public void setConnected(boolean connected) {
+        this.connected = connected;
+    }
+
     public PacketManager getPacketManager() {
         return packetManager;
+    }
+
+    public void setPacketManager(PacketManager packetManager) {
+        this.packetManager = packetManager;
     }
 
     public String getNickname() {
@@ -22,5 +36,6 @@ public class User extends Model {
     public User(String nickname, PacketManager packetManager) {
         this.nickname = nickname;
         this.packetManager = packetManager;
+        this.connected = true;
     }
 }
