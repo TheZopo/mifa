079c3185a03d5164f0e6ce290c60550bbff9568e
Bastien Marsaud <bastien@mouseover.fr>
2019-10-02 21:36:25 +0200

    wip - adds room packet manager

--- src/main/java/fr/mifa/core/network/protocol/JoinedRoomPacket.java
+++ src/main/java/fr/mifa/core/network/protocol/JoinedRoomPacket.java
@@ -5,7 +5,15 @@ import fr.mifa.core.models.Room;
 public class JoinedRoomPacket extends Packet {
     private Room room;
 
+    private String address;
+    private int port;
+
     private String nickname;
+
+    public JoinedRoomPacket(String nickname, Room room) {
+        this.room = room;
+        this.nickname = nickname;
+    }
 
     public Room getRoom() {
         return room;
@@ -23,8 +31,19 @@ public class JoinedRoomPacket extends Packet {
         this.nickname = nickname;
     }
 
-    public JoinedRoomPacket(String nickname, Room room) {
-        this.room = room;
-        this.nickname = nickname;
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
     }
 }
