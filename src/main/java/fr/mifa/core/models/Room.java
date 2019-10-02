c748991d20bc84c59021d1e6d519adf2c973ab40
Bastien Marsaud <bastien@mouseover.fr>
2019-10-02 21:34:18 +0200

    Revert "wip - adds room packet manager"
    This reverts commit 075719f647bd2e4037d947bfb42dfd2a2d26ea42.

--- src/main/java/fr/mifa/core/models/Room.java
+++ src/main/java/fr/mifa/core/models/Room.java
@@ -1,24 +1,14 @@
 package fr.mifa.core.models;
 
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.MulticastSocket;
 import java.util.ArrayList;
 
-import fr.mifa.core.network.RoomPacketManager;
-
 public class Room extends Model {
+
     private String name;
+
     private ArrayList<User> users;
+
     private ArrayList<Message> history;
-
-    private transient RoomPacketManager packetManager;
-
-    public Room(String name) {
-        this.users = new ArrayList<>();
-        this.name = name;
-        this.history = new ArrayList<>();
-    }
 
     public String getName() {
         return name;
@@ -44,12 +34,9 @@ public class Room extends Model {
         this.history = history;
     }
 
-    public RoomPacketManager getPacketManager() {
-        return packetManager;
-    }
-
-    public void setPacketManager(RoomPacketManager packetManager) {
-        this.packetManager = packetManager;
-        this.packetManager.start();
+    public Room(String name) {
+        this.users = new ArrayList<>();
+        this.name = name;
+        this.history = new ArrayList<>();
     }
 }
--- src/main/java/fr/mifa/core/network/protocol/JoinedRoomPacket.java
+++ src/main/java/fr/mifa/core/network/protocol/JoinedRoomPacket.java
@@ -5,15 +5,7 @@ import fr.mifa.core.models.Room;
 public class JoinedRoomPacket extends Packet {
     private Room room;
 
-    private String address;
-    private int port;
-
     private String nickname;
-
-    public JoinedRoomPacket(String nickname, Room room) {
-        this.room = room;
-        this.nickname = nickname;
-    }
 
     public Room getRoom() {
         return room;
@@ -31,19 +23,8 @@ public class JoinedRoomPacket extends Packet {
         this.nickname = nickname;
     }
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public int getPort() {
-        return port;
-    }
-
-    public void setPort(int port) {
-        this.port = port;
+    public JoinedRoomPacket(String nickname, Room room) {
+        this.room = room;
+        this.nickname = nickname;
     }
 }
